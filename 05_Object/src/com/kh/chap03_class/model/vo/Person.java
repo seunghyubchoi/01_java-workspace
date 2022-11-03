d6371a0cfca566cb1c484f3bc35f36fb07ce6e9b
seunghyubchoi <seunghyub.choi@gmail.com>
2022-11-03 21:04:56 +0900

    setter getter practice

--- 05_Object/src/com/kh/chap03_class/model/vo/Person.java
+++ 05_Object/src/com/kh/chap03_class/model/vo/Person.java
@@ -1,77 +1,42 @@
 package com.kh.chap03_class.model.vo;
 
 public class Person {
-
-	// 1. 필드 선언
-	
-	private String id;
-	private String pwd;
 	private String name;
 	private int age;
+	private double height;
 	private char gender;
-	private String phone;
-	private String email;
-
-	// 나이, 성별, 전화번호, 이메일 => age, gender, phone(-포함), email
-
-	// 메소드 선언
-
-	// 2. setter 만들기 (setter는 반환값 없고 매개변수 있음, this 적어주기)
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
+	private int birth;
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
+	public void setHeight(double height) {
+		this.height = height;
+		
+	}
 	public void setGender(char gender) {
 		this.gender = gender;
 	}
-	
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	// 3. getter 만들기 (getter는 반환값 있고 매개변수 없음, return 적어주기)
-
-	public String getId() {
-		return id;
-	}
-	
-	public String getPwd() {
-		return pwd;
+	public void setBirth(int birth) {
+		this.birth = birth;
 	}
 	
 	public String getName() {
 		return name;
 	}
-	
 	public int getAge() {
 		return age;
 	}
-	
+	public double getHeight() {
+		return height;
+	}
 	public char getGender() {
 		return gender;
 	}
-	
-	public String getPhone() {
-		return phone;
-	}
-	
-	public String getEmail() {
-		return email;
+	public int getBirth() {
+		return birth;
 	}
 }
--- 05_Object/src/com/kh/chap03_class/run/ClassRun.java
+++ 05_Object/src/com/kh/chap03_class/run/ClassRun.java
@@ -1,29 +1,20 @@
 package com.kh.chap03_class.run;
+
 import com.kh.chap03_class.model.vo.*;
+
 public class ClassRun {
 	public static void main(String[] args) {
-		
-		// 4. 객체 만들기
-		
 		Person p1 = new Person();
+		p1.setName("케로로");
+		p1.setAge(1900);
+		p1.setHeight(120.5);
+		p1.setGender('M');
+		p1.setBirth(118);
 		
-		// 생성 후에 필드에 담긴 값을 곧바로 알아보면
-		// JVM이 초기화(값 세팅)까지 진행해줬음!!
-		
-		p1.setId("bonobono");
-		p1.setPwd("pptmaster");
-		p1.setName("보노보노");
-		p1.setAge(30);
-		p1.setGender('m');
-		p1.setPhone("02-3288-8080");
-		p1.setEmail("bono@bono.com");
-				
-		System.out.println("아이디 : " + p1.getId());
-		System.out.println("비밀번호 : " + p1.getPwd());
-		System.out.println("이름 : " + p1.getName());
-		System.out.println("나이 : " + p1.getAge());
-		System.out.println("성별 : " + p1.getGender());
-		System.out.println("전화번호 : " + p1.getPhone());
-		System.out.println("이메일 : " + p1.getEmail());
+		System.out.println(p1.getName());
+		System.out.println(p1.getAge());
+		System.out.println(p1.getHeight());
+		System.out.println(p1.getGender());
+		System.out.println(p1.getBirth());
 	}
 }
