d7393d38e441754227e6097b3bb2ece1e225676e
seunghyubchoi <seunghyub.choi@gmail.com>
2022-11-11 07:22:28 +0900

    test

--- TestProject/src/com/kh/test/Test.java
+++ TestProject/src/com/kh/test/Test.java
@@ -1,7 +1,21 @@
 package com.kh.test;
 
-public class Test {
-	public void method1(){
-		System.out.println("하이루");
-	}
+class Parents{
+    public String x(){return "A.x";}
+}
+class Child1 extends Parents{
+    public String x(){return "B.x";}
+    public String y(){return "y";}
+}
+class Child2 extends Parents{
+    public String x(){return "B2.x";}
+}
+
+public class test {
+    public static void main(String[] args) {
+    	Parents obj = new Child1();
+    	Parents obj2 = new Child2();
+        System.out.println(obj.x());
+        System.out.println(obj2.x());
+    }
 }
