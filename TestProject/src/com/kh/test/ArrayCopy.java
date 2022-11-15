d2d820d8a06f7b0234b798adf8b4ed43e7b87633
seunghyubchoi <seunghyub.choi@gmail.com>
2022-11-15 15:18:37 +0900

    Update ArrayCopy.java

--- TestProject/src/com/kh/test/ArrayCopy.java
+++ TestProject/src/com/kh/test/ArrayCopy.java
@@ -22,16 +22,20 @@ public class ArrayCopy {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String[] strArray = { "바나나 ", "사과 " };
+		 
+		String[] strArray = { "뉴칼레도니아 ", "사과 " };
 		// 배열과 변수를 매개변수로 하는 메소드 호출
 		System.out.println("입력 값 : ");
 		String input = sc.nextLine() + " ";
+		
+		
+		char[] str2 = strArray[0].toCharArray();
 		
 		strArray = add(strArray, input);
 		
 		
 		System.out.println(Arrays.toString(strArray));
-		
+		System.out.println(str2[(int)(Math.random() * str2.length)]);
 		
 
 	}
